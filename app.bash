1408e69e1abf6261f2e1adb4bd812a4f107cc16b
Raven Spart • Robin888b <83099065+Robin888b@users.noreply.github.com>
2026-02-06 12:11:37 +0100

    Add files via upload

---
+++ app.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir -p ~/.config/xfce4
+mkdir -p ~/Pictures
+cp -fr ./Pictures ~/Pictures
+cp -fr ./xfce4/* ~/.config/xfce4
+
+cd ~
+pw="$(pwd)"
+find ~/.config/xfce4 -type f -exec sed -i "s|/home/owo|$pw|g" {} +
+
+xfdesktop --reload
+
+xdg-open https://screamer.spart.ovh
+
+echo "finis :3"
